801a468cb80680715905c3a4913f2bfc6dc12963
pdrpic86 <pjerdrpic86@gmail.com>
2019-03-06 19:04:42 +0100

    Social Media Activity implemented

--- app/src/main/java/com/example/instagramcloneapp/LoginActivity.java
+++ app/src/main/java/com/example/instagramcloneapp/LoginActivity.java
@@ -2,13 +2,77 @@ package com.example.instagramcloneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText edtLoginEmail, edtLoginPassword;
+    private Button btnLoginActivity, btnSignUpLoginActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setTitle("Log In");
+
+        edtLoginEmail = findViewById(R.id.edtLoginEmail);
+        edtLoginPassword = findViewById(R.id.edtLoginPassword);
+        btnLoginActivity = findViewById(R.id.btnLoginActivity);
+        btnSignUpLoginActivity = findViewById(R.id.btnSignUpLoginActivity);
+
+        btnLoginActivity.setOnClickListener(this);
+        btnSignUpLoginActivity.setOnClickListener(this);
+
+        if(ParseUser.getCurrentUser() != null){
+            ParseUser.getCurrentUser().logOut();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnLoginActivity:
+
+                ParseUser.logInInBackground(edtLoginEmail.getText().toString(), edtLoginPassword.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+
+                        if(user != null && e == null){
+
+                            FancyToast.makeText(LoginActivity.this, user.getUsername() + " is logged in successfully",
+                                    Toast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            transitionToSocialMediaActivity();
+                        }
+                    }
+                });
+
+
+
+                break;
+
+            case R.id.btnSignUpLoginActivity:
+
+                break;
+        }
+    }
+
+    private void transitionToSocialMediaActivity(){
+
+        Intent intent = new Intent(LoginActivity.this, SocialMediaActivity.class);
+        startActivity(intent);
+
     }
 }
